5671d3ee78c908859e6cbab9d9a86c4a5762a907
Rajesh007 <mallepogurajesh.9999@gmail.com>
2023-06-12 17:02:01 +0530

    hello

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+function validateForm(event) {
+  var aadhaarNumber = document.getElementById("aadhar").value;
+  var contact1 = document.getElementById("contact-1").value;
+  var contact2 = document.getElementById("contact-2").value;
+  var aadhaarPattern = /^\d{4}\s\d{4}\s\d{4}$/; // Aadhaar card format: XXXX XXXX XXXX
+  var contactPattern = /^\d{10}$/; // Contact number format: 10 digits
+  var errors = 0;
+
+  var aadhaarError = document.getElementById("aadhaarError");
+  if (!aadhaarPattern.test(aadhaarNumber)) {
+    aadhaarError.textContent = "Please enter a valid Aadhaar card number in the format XXXX XXXX XXXX.";
+    errors++;
+  } else {
+    aadhaarError.textContent = "";
+  }
+
+  var contact1Error = document.getElementById("contact1Error");
+  if (!contactPattern.test(contact1)) {
+    contact1Error.textContent = "Please enter a valid 10-digit contact number for Contact 1.";
+    errors++;
+  } else {
+    contact1Error.textContent = "";
+  }
+
+  var contact2Error = document.getElementById("contact2Error");
+  if (!contactPattern.test(contact2)) {
+    contact2Error.textContent = "Please enter a valid 10-digit contact number for Contact 2.";
+    errors++;
+  } else {
+    contact2Error.textContent = "";
+  }
+
+  if (errors > 0) {
+    event.preventDefault();
+  }
+}
--- server.js
+++ server.js
@@ -1,49 +1,99 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const session = require("express-session")
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+app.use(session({
+    secret: 'PRAGATHI12345',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.post("/admission", function(req, res){
+    const formData = req.body;
+    const formattedData = `Name: ${formData.studentname}\nMother Name: ${formData.mothername}\nFather Name: ${formData.fathername}\nStudent age: ${formData.age}\nDOB: ${formData.dob}\nAdmission Class: ${formData.admissioninto}\nAddress: ${formData.address}\nAadhar No: ${formData.aadhar}\nContact-1: ${formData.contact1}\nContact-2: ${formData.contact2}`;
+
+    sendEmail(formattedData)
+        .then(() => {
+            // Set a flag to indicate successful form submission
+            req.session.submitted = true;
+            res.redirect('/admission');
+        })
+        .catch((error) => {
+            console.error('Error sending email:', error);
+            // Set an error flag to indicate form submission error
+            req.session.error = true;
+            res.redirect('/admission');
+        });
+});
+
+
+function sendEmail(content) {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'pragathiutility@gmail.com', // replace with your email address
+            pass: 'tqbntkujmegkesmu' // replace with your email password
+        }
+    });
+
+    const mailOptions = {
+        from: 'pragathiutility@gmail.com', // replace with your email address
+        to: 'pragathiutility@gmail.com', // replace with the recipient email address
+        subject: 'New Admission',
+        text: content
+    };
+
+    return transporter.sendMail(mailOptions);
+}
+
 
 app.get("/", function(req, res){
-    res.render("index")
+    res.render("index");
 });
 
 app.get("/about", function(req, res){
-    res.render("about")
+    res.render("about");
 });
 
 app.get("/contact", function(req, res){
-    res.render("contact")
+    res.render("contact");
 });
 
-app.get("/admission", function(req, res){
-    res.render("admission")
+app.get('/admission', function(req, res) {
+    const submitted = req.session.submitted;
+    const error = req.session.error;
+    delete req.session.submitted;
+    delete req.session.error;
+    res.render('admission', { submitted, error });
 });
 
 app.get("/gallery", function(req, res){
-    res.render("gallery")
+    res.render("gallery");
 });
 
 app.get("/fee", function(req, res){
-    res.render("fee")
+    res.render("fee");
 });
 
 app.get("/calendar", function(req, res){
-    res.render("calendar")
+    res.render("calendar");
 });
 
 app.get("/circulars", function(req, res){
-    res.render("circulars")
+    res.render("circulars");
 });
 
 app.get("/ssc-results", function(req, res){
-    res.render("ssc-results")
+    res.render("ssc-results");
 });
 
 app.get("/student-login", function(req, res){
-    res.render("student-login")
+    res.render("student-login");
 });
 
 
